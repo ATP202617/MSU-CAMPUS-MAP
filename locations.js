8a35bf657585545363e74b9ddaa029c5cccf3a29
ATP202617 <atptamano@gmail.com>
2026-07-26 20:13:41 +0800

    Update locations.js

--- locations.js
+++ locations.js
@@ -1,40 +1,40 @@
 const locations = [
   {
-    name: "Main Gate",
+    name: "✅Main Gate",
     x: 1456,
     y: 662,
-    description: "Main entrance of MSU.",
-    icon: "gate"
+    description: "✅Main entrance of MSU.",
+    icon: "office"
   },
  
   
   
   {
-    name: "College of Agriculture",
+    name: "✅College of Agriculture",
     x: 233,
     y: 64,
     description: "",
     icon: "office"
   },
   {
-    name: "King Faisal Center for Islamic, Arabic and Asian Studies",
+    name: "✅King Faisal Center for Islamic, Arabic and Asian Studies",
     x: 1232,
     y: 738,
     category: " ",
     description: "",
     icon: "office",
-    hours: "8:00am - 5:00pm",
+   // hours: "",
     image: 'images/kfciaaas.jpg'
   },
   {
-    name: "College of Forestry and Environmental Studies",
+    name: "✅College of Forestry and Environmental Studies",
     x: 949,
     y: 769,
     description: "",
     icon: "office"
    },
    {
-    name: "College of Fisheries and Aquatic Sciences",
+    name: "✅College of Fisheries and Aquatic Sciences",
     x: 834,
     y: 878,
     description: "",
@@ -42,7 +42,7 @@ const locations = [
     },
 
     {
-    name: "Colleg of Hospitality and Tourism Management",
+    name: "✅College of Hospitality and Tourism Management",
     x: 668,
     y:898,
     description: "",
@@ -50,7 +50,7 @@ const locations = [
     },
 
     {
-    name: "Medical Services and Hospital",
+    name: "✅Medical Services and Hospital",
     x: 272,
     y: 894,
     description: "",
@@ -58,14 +58,14 @@ const locations = [
     },
 
     {
-    name: "Rajah Indarapatra Hall",
+    name: "✅Rajah Indarapatra Hall",
     x: 372,
     y: 836,
     description: "",
     icon: "office"
 },
     {
-    name: "College of Information and Computing Sciences",
+    name: "✅College of Information and Computing Sciences",
     x: 335,
     y: 784,
     description: "",
@@ -73,63 +73,63 @@ const locations = [
 },
 
     {
-    name: "International Convention Center",
+    name: "✅International Convention Center",
     x: 296,
     y: 753,
     description: "",
     icon: "office"
 },
 {
-    name: "Antonia Isidro Hall",
+    name: "✅Antonio Isidro Hall",
     x: 387,
     y: 750,
     description: "",
     icon: "office"
 },
 {
-    name: "Ahmad Domocao Alonto Sr. Bldg.",
+    name: "✅Ahmad Domocao Alonto Sr. Bldg.",
     x: 456,
     y: 731,
     description: "",
     icon: "office"
 },
 {
-    name: "University Library",
+    name: "✅University Library",
     x: 537,
     y: 712,
     description: "",
     icon: "office"
 },
 {
-    name: "Aga Khan Museum",
+    name: "✅Aga Khan Museum",
     x: 400,
     y: 644,
     description: "",
     icon: "office"
 },
 {
-    name: "Information and Communications Technology Center",
+    name: "✅Information and Communications Technology Center",
     x: 500,
     y: 636,
     description: "",
     icon: "office"
 },
 {
-    name: "College of Natural Sciences and Mathematics",
+    name: "✅College of Natural Sciences and Mathematics",
     x: 582,
     y: 536,
     description: "",
     icon: "office"
 },
 {
-    name: "College of Social Sciences and Humanities",
+    name: "✅College of Social Sciences and Humanities",
     x: 585,
     y: 473,
     description: "",
     icon: "office"
 },
 {
-    name: "Bolawan Hall",
+    name: "✅Bolawan Hall",
     x: 884,
     y: 276,
     description: "",
@@ -137,138 +137,138 @@ const locations = [
 
 },
 {
-    name: "Office of information, press and publication",
+    name: "✅Office of information, press and publication",
     x: 745,
     y: 62,
     description: "",
     icon: "office"
 },
 {
-    name: "Khadija Dormitory",
+    name: "✅Khadija Dormitory",
     x: 658,
     y: 42,
     description: "",
     icon: "office"
 },
 {
-    name: "Potre Maamor Hall",
+    name: "✅Potre Maamor Hall",
     x: 602,
     y: 56,
     description: "",
     icon: "office"
 },
 {
-    name: "Rajah Solaiman Dormitory",
+    name: "✅Rajah Solaiman Dormitory",
     x: 674,
     y: 254,
     description: "",
     icon: "office"
 },
 {
-    name: "Super New Boys Dormitory",
+    name: "✅Super New Boys Dormitory",
     x: 591,
     y: 301,
     description: "",
     icon: "office"
 },
 {
-    name: "Super New Girls Dormitory",
+    name: "✅Super New Girls Dormitory",
     x: 528,
     y: 266,
     description: "",
     icon: "office"
 },
 {
-    name: "College of Public Affairs",
+    name: "✅College of Public Affairs",
     x: 428,
     y: 257,
     description: "",
     icon: "office"
 },
 {
-    name: "College of Education",
+    name: "✅College of Education",
     x: 457,
     y: 134,
     description: "",
     icon: "office"
 },
 {
-    name: "College of Engineering",
+    name: "✅College of Engineering",
     x: 638,
     y: 154,
     description: "",
     icon: "office"
 },
 {
-    name: "College of Law",
+    name: "✅College of Law",
     x: 384,
     y: 101,
     description: "",
     icon: "office"
 },
 {
-    name: "College of Business Administration and Accountancy",
+    name: "✅College of Business Administration and Accountancy",
     x: 317,
     y: 153,
     description: "",
     icon: "office"
 },
 {
-    name: "Dimaporo Gymnasium",
+    name: "✅Dimaporo Gymnasium",
     x: 140,
     y: 248,
     description: "",
     icon: "office"
 },
 {
-    name: "College of Sports, Physical Education, and Recreation",
+    name: "✅College of Sports, Physical Education, and Recreation",
     x: 192,
     y: 330,
     description: "",
     icon: "office"
 },
 {
-    name: "Department of Security Services",
+    name: "✅Department of Security Services",
     x: 136,
     y: 462,
     description: "",
     icon: "office"
 },
 {
-    name: "Princess Lawanen Hall",
+    name: "✅Princess Lawanen Hall",
     x: 287,
     y: 564,
     description: "",
     icon: "office"
 },
 {
-    name: "Botanical Garden",
+    name: "✅Botanical Garden",
     x: 289,
     y: 654,
     description: "",
     icon: "office"
 },
 {
-    name: "Presidential House",
+    name: "✅Presidential House",
     x: 648,
     y: 844,
-    description: "Presidential House",
+    description: "",
     icon: "office"
 },
 
 {
-    name: "College of Medicine",
+    name: "✅College of Medicine",
     x: 1434,
     y: 780,
-    description: "COM",
+    description: "",
     icon: "office"
 },
 
 {
-    name: "College of Health and Sciences",
+    name: "✅College of Health and Sciences",
     x: 1433,
     y: 858,
-    description: "CHS",
+    description: "",
     icon: "office"
 },
 
